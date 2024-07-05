24bf5e3096d1a7049895f315ed4c1cbd65021123
Otto Akseli Hannuksela <otto.akseli.hannuksela@gmail.com>
2024-07-05 09:54:14 +0800

    Updated

--- pilot_examples/example_lens_reconstruction.py
+++ pilot_examples/example_lens_reconstruction.py
@@ -1,8 +1,19 @@
 import corner
 import numpy as np
+import jax
+import jax.numpy as np
 import pylab as plt
 from scipy.stats import multivariate_normal
 
+def calculate_fim_matrix_from_image_data(theta_l_true):
+    # Compute the Hessian matrix
+    hessian = jax.hessian(chi_sq)(theta_l_true)
+    # Compute the Fisher Information Matrix
+    fim_matrix = jnp.linalg.inv(hessian)
+    
+
+# FIM STEP
+fim_matrix = calculate_fim_matrix_from_image_data(true_values)
 
 # PLOT STEP
 # # Sample from the multivariate normal distribution
