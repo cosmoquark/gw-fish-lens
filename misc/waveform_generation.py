cb3aec0fd589221264aad1bccc532f98c3e5b4a9
Otto Akseli Hannuksela <otto.akseli.hannuksela@gmail.com>
2024-08-21 14:39:11 +0800

    Updated

--- misc/waveform_generation.py
+++ misc/waveform_generation.py
@@ -4,13 +4,16 @@ from pycbc.detector import Detector, get_available_detectors
 import pylab as plt
 import numpy as np
 
-def create_whitened_waveform(phase=np.pi/2, inclination=np.pi/2, polarization=0.1, t_gps = 1126259462.0, mass1=80, mass2=80, spin1x=0, spin1y=0, spin1z=0, spin2x=0, spin2y=0, spin2z=0, approximant = "SEOBNRv4PHM"):
+def create_whitened_waveform(phase=np.pi/2, inclination=np.pi/2, polarization=0.7, t_gps = 1126259462.0, mass1=80, mass2=80, 
+                             spin1x=0, spin1y=0, spin1z=0, spin2x=0, spin2y=0, spin2z=0, approximant = "SEOBNRv4PHM"):
     # Create a whitened waveform in time domain
     # phase = np.pi/2
     # inclination = np.pi/2
     # hp, hc = waveform.get_td_waveform(approximant="IMRPhenomXPHM", mass1=80, mass2=80, delta_t=1.0/4096, f_lower=10, distance=2000, inclination=inclination, coa_phase=phase)
-    hp, hc = waveform.get_td_waveform(approximant=approximant, mass1=mass1, mass2=mass2, delta_t=1.0/4096, f_lower=10, distance=2000, inclination=inclination, coa_phase=phase, 
-                                      spin1x=spin1x, spin1y=spin1y, spin1z=spin1z, spin2x=spin2x, spin2y=spin2y, spin2z=spin2z)
+    hp, hc = waveform.get_td_waveform(approximant=approximant, mass1=mass1, mass2=mass2, delta_t=1.0/4096, f_lower=10, distance=2000, 
+                                      inclination=inclination, coa_phase=phase, 
+                                      spin1x=spin1x, spin1y=spin1y, spin1z=spin1z, spin2x=spin2x, spin2y=spin2y, spin2z=spin2z,
+                                      polarization=polarization)
     # Set the hp and hc start time to the GPS time
     hp.start_time = hc.start_time = t_gps
     # Compute the plus and cross polarizations
